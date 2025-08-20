73630d5bff296a56c6fbc3c948480450040b08b0
Guilherme1ds <bernardiguilherme24@gmail.com>
2025-08-20 16:27:49 -0300

    update 20/08

--- Aula 05/AtividadePOO.php
+++ Aula 05/AtividadePOO.php
@@ -18,15 +18,15 @@ class Cachorro {
 
 // Exercício 2: Após a criação da classe do exercício 1, crie 10 cachorros (10 objetos).
 
-$cachorro1 = new Cachorro("Princesa", "2 Anos", "Pinscher", "Não", "Fêmea");
-$cachorro2 = new Cachorro("Scooby", "3 Anos", "Salsicha", "Sim", "Macho");
-$cachorro3 = new Cachorro("Linguiça", "5 Anos", "Shitzu", "Sim", "Macho");
-$cachorro4 = new Cachorro("Felpuda", "4 Meses", "Poodle", "Não", "Fêmea");
-$cachorro5 = new Cachorro("Bolas", "6 Anos", "Pitbull", "Não", "Macho");
-$cachorro6 = new Cachorro("Zeus", "7 Anos", "Pastor Alemão", "Sim", "Macho");
-$cachorro7 = new Cachorro("Pérola", "1 Ano", "Pug", "Sim", "Fêmea");
-$cachorro8 = new Cachorro("Torpedo", "9 Anos", "Bulldog", "Não", "Macho");
-$cachorro9 = new Cachorro("Dourado", "10 Anos", "Golden Retriever", "Sim", "Macho");
-$cachorro10 = new Cachorro("Jezer", "11 Anos", "Border Collie", "Sim", "Macho");
+$cachorro1 = new Cachorro("Princesa", "2 Anos", "Pinscher", false, "Fêmea");
+$cachorro2 = new Cachorro("Scooby", "3 Anos", "Salsicha", true, "Macho");
+$cachorro3 = new Cachorro("Linguiça", "5 Anos", "Shitzu", true, "Macho");
+$cachorro4 = new Cachorro("Felpuda", "4 Meses", "Poodle", false, "Fêmea");
+$cachorro5 = new Cachorro("Bolas", "6 Anos", "Pitbull", false, "Macho");
+$cachorro6 = new Cachorro("Zeus", "7 Anos", "Pastor Alemão", true, "Macho");
+$cachorro7 = new Cachorro("Pérola", "1 Ano", "Pug", true, "Fêmea");
+$cachorro8 = new Cachorro("Torpedo", "9 Anos", "Bulldog", false, "Macho");
+$cachorro9 = new Cachorro("Dourado", "10 Anos", "Golden Retriever", true, "Macho");
+$cachorro10 = new Cachorro("Jezer", "11 Anos", "Border Collie", true, "Macho");
 
 ?>
